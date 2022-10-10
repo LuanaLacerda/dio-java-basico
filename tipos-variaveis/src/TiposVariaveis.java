02f550b0b3a60852bf562541903fb514c16590c4
Luana Lacerda <luanaretifica@hotmail.com>
2022-10-10 13:45:14 -0300

    tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        final double VALOR_DE_PI = 3.14; // constante não altera valor usar 'final', esta variável deverá ser escrita
+                                         // toda em caixa alta.
+
     }
 }
